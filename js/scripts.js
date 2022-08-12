1e0bbcb534fcb343e49060787b6aa0d7f4e18698
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-12 11:45:13 -0700

    finally got the javascript working still WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
+function codename(event) {
+  event.preventDefault();
+  let a = document.querySelector("select#question1").value;
+  let b = document.querySelector("select#question2").value;
+  let c = document.querySelector("select#question3").value;
+  let d = document.querySelector("select#question4").value;
+  let e = document.querySelector("select#question5").value;
+
+    if (a === "yes1")(b === "no2")(c === "no3")(d === "no4")(e === "no5"); {
+        return "Javascript"
+      } 
+    };
+
 window.addEventListener("load", function() {
   const form = document.getElementById("questions")
-  document.querySelector("form").onsubmit = function(e) {
-    e.preventDefault();
-  let a = document.querySelector("1", "2").value;
-  let b = parseInt(document.querySelector("input#inputb").value);
-  let c = parseInt(document.querySelector("input#inputc").value);
-  const result = checkTri(a, b, c);
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+  const result = codename(event);
   document.getElementById("output").innerText = result;
   }
 });
