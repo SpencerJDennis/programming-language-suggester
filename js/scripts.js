89c42a59581981c029b3869f40aa0a0f8ebcaff7
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-12 16:29:22 -0700

    basically scrapped js code to make a new one

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,25 @@
 function codename(e) {
-  let a = document.querySelector("select#question1").value;
-  let b = document.querySelector("select#question2").value;
-  let c = document.querySelector("select#question3").value;
-  let d = document.querySelector("select#question4").value;
-  let f = document.querySelector("select#question5").value;
-console.log("did it run")
-    if (a === "yes1" || b != "yes2" || c != "yes3" || d != "yes4" || f != "yes5") {
-      return "Javascript";
-    }else if (a === "no1" || b === "yes2" || c === "no3" || d === "no4" || f === "no5") {
-      return "C#";
-    }else if (a != "yes1" || b != "yes2" || c === "yes3" || d != "yes4" || f != "yes5") {
-      return "python";
-    }else if (a != "yes1" || b != "yes2" || c != "yes3" || d === "yes4" || f != "yes5") {
-      return "You can't learn all three start with one!WELL GO TAKE A DIFFERENT SURVEY THEN!!!";
-    }else if (a != "yes1" || b != "yes2" || c != "yes3" || d != "yes4" || f === "yes5") {
-      return "You can't learn all three start with one!";
-    }else  {
-      return "error";
-    }
-  };
+  e.preventDefault();
+  let a = document.getElementById("question1").value;
+
+  if (a === "java") {
+    return "Javascript";
+  }else if (a === "csharp") {
+    return "C#";
+  }else if (a === "python") {
+    return "python";
+  }
+}
 
 window.addEventListener("load", function() {
-  const form = document.getElementById("questions");
+  const form = document.getElementById("question1");
   document.querySelector("form").onsubmit = function(e) {
     e.preventDefault(); 
-  const result = codename(e);
+    
+    const result = codename(e);
   document.getElementById("output").innerText = result;
   };
 });
+
+
 
