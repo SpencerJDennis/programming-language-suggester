f4fea4f7df3a2a315fad8ccc5125cee048643baf
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-12 15:05:58 -0700

    I can't seem to get this function to work still trying WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
-function codename(event) {
-  event.preventDefault();
+function codename(e) {
   let a = document.querySelector("select#question1").value;
   let b = document.querySelector("select#question2").value;
   let c = document.querySelector("select#question3").value;
   let d = document.querySelector("select#question4").value;
-  let e = document.querySelector("select#question5").value;
-
-    if (a === "yes1")(b === "no2")(c === "no3")(d === "no4")(e === "no5"); {
-      return "Javascript"
-      
-    }else if (a === "no1")(b === "yes2")(c === "no3")(d === "no4")(e === "no5"); {
-      return "Javascript"
-    
+  let f = document.querySelector("select#question5").value;
+console.log("did it run")
+    if (a === "yes1" || b != "yes2" || c != "yes3" || d != "yes4" || f != "yes5") {
+      return "Javascript";
+    }else if (a === "no1" || b === "yes2" || c === "no3" || d === "no4" || f === "no5") {
+      return "C#";
+    }else if (a != "yes1" || b != "yes2" || c === "yes3" || d != "yes4" || f != "yes5") {
+      return "python";
+    }else if (a != "yes1" || b != "yes2" || c != "yes3" || d === "yes4" || f != "yes5") {
+      return "You can't learn all three start with one!WELL GO TAKE A DIFFERENT SURVEY THEN!!!";
+    }else if (a != "yes1" || b != "yes2" || c != "yes3" || d != "yes4" || f === "yes5") {
+      return "You can't learn all three start with one!";
+    }else  {
+      return "error";
     }
   };
 
 window.addEventListener("load", function() {
-  const form = document.getElementById("questions")
-  document.querySelector("form").onsubmit = function(event) {
-    event.preventDefault();
-  const result = codename(event);
+  const form = document.getElementById("questions");
+  document.querySelector("form").onsubmit = function(e) {
+    e.preventDefault(); 
+  const result = codename(e);
   document.getElementById("output").innerText = result;
-  }
+  };
 });
+
